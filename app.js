7bb1d5d4abff494bb570446f239eaac9d91ed3dd
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2021-03-29 20:47:38 +0600

    Task list Completed. Txt Showing error Remains

--- app.js
+++ app.js
@@ -21,39 +21,84 @@ function addTask(e) {
   if (taskInput.value !== "") {
     const parent = document.querySelector(".task-collection");
     const child = document.createElement("li");
-    child.innerHTML = `<span>${taskInput.value}</span>
-                       <a class="right" href="">
+    child.innerHTML = `<span class="task-name">${taskInput.value}</span>
+                       <a class="right" href="#">
                        <i class="material-icons m-0" id="task-remove">remove_circle</i>
                       </a>
-                      <a class="right" href="">
+                      <a class="right" href="#">
                       <i class="material-icons m-0" id="task-done">check_box
                       </i>
                       </a>`;
     child.classList.add("collection-item");
     parent.appendChild(child);
+    showAlert(".task-input-field", "Task added to the list!", "sucess-alert");
     taskInput.value = "";
-    console.log(taskInput.value);
   } else {
-    const parent = document.querySelector(".task-input-field");
-    const alertTxt = document.createElement("p");
-    alertTxt.innerHTML = `<p class="input-task-alert">Enter A Task First!</p>`;
-    parent.appendChild(alertTxt);
+    showAlert(".task-input-field", "Enter a task first!", "error-alert");
+  }
+}
 
-    setTimeout(function remove() {
-      alertTxt.remove();
-    }, 5000);
+document.body.addEventListener("click", taskControl);
+
+function taskControl(e) {
+  if (e.target.id == "task-done") {
+    const tasks = Array.from(e.target.parentElement.parentElement.children);
+
+    tasks.forEach((task) => {
+      if (task.classList.contains("task-name")) {
+        if (!task.classList.contains("done-task")) {
+          task.classList.add("done-task");
+          e.target.style.color = "grey";
+
+          showAlert(".card-content", "Task marked as done!", "sucess-alert");
+        } else {
+          task.classList.remove("done-task");
+          e.target.style.color = "#5e35b1";
+          showAlert(".card-content", "Task marked as not done!", "error-alert");
+        }
+      }
+    });
   }
 
-  /**
-   *  <li class="collection-item"><span>Task 1</span>
-                                <a class="right" href=""><i class="material-icons m-0" id="task-remove">remove_circle
-                                    </i>
-                                </a>
-                                <a class="right" href=""><i class="material-icons m-0" id="task-done">check_box
-                                    </i>
-                                </a>
-                            </li>
-   */
+  if (e.target.id == "task-remove") {
+    const listItem = e.target.parentElement.parentElement;
+
+    listItem.remove();
+  }
+}
+
+function showAlert(parentClassName, msg, styleClassName) {
+  const alert = document.createElement("p");
+
+  alert.classList.add(styleClassName);
+  alert.appendChild(document.createTextNode(msg));
+  const parent = document.querySelector(`${parentClassName}`);
+
+  parent.appendChild(alert);
+
+  setTimeout(function remove() {
+    alert.remove();
+  }, 1000);
+}
+
+const dltAllTaskBtn = document.querySelector("#dltall-btn");
+dltAllTaskBtn.addEventListener("click", deleteAllTask);
+
+function deleteAllTask(e) {
+  const taskLists = Array.from(
+    document.querySelector(".task-collection").children
+  );
+
+  if (taskLists.length !== 0) {
+    taskLists.forEach((taskList) => {
+      if (taskList.classList.contains("collection-item")) {
+        taskList.remove();
+      }
+    });
+    showAlert(".task-input-field", "All tasks deleted!", "sucess-alert");
+  } else {
+    showAlert(".task-input-field", "No task to delete!", "error-alert");
+  }
 }
 /*const main_btn = document.querySelector("#m-play-btn");
 const main_stopBtn = document.querySelector("#m-stop-btn");
